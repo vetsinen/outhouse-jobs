bff908b4f33650620e1925e6d2de6dcc8be936bb
vetsinen <vetsinen@gmail.com>
2020-05-04 10:10:15 +0300

    mysterios navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,32 @@
 <template>
   <div>
-    <nav class="fixed-bottom navbar-expand navbar-light bg-light">
-      <div>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
-              Catalogue
-            </nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link active-class="active" class="nav-link" to="/orders">
-              Orders
-            </nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link active-class="active" class="nav-link" to="/profile">
-              Profile
-            </nuxt-link>
-          </li>
-        </ul>
-      </div>
+    <nav class="row flex-row flex-nowrap fixed-bottom"
+         style="font-size:3em; background-color: white; margin: 15px 5px">
+      <nuxt-link active-class="active" class="nav-link col" to="/" title="Catalogue">
+        &#127911;
+      </nuxt-link>
+      <nuxt-link active-class="active col-4" class="nav-link col" to="/orders" title="Orders">
+        &#9874;
+      </nuxt-link>
+      <nuxt-link active-class="active" class="nav-link col" to="/profile" title="Profile">
+        &#128084;
+      </nuxt-link>
     </nav>
-    <nuxt />
+
+    <nuxt/>
   </div>
 </template>
 
+<script>
+  // <nuxt-link active-class="active" class="nav-link" to="/profile">
+</script>
+
 <style>
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 5px;
-}
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 10px;
+  }
 
 </style>
