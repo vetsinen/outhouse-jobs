57ba385e437325334acc5cf2130457ae264b3098
vetsinen <vetsinen@gmail.com>
2020-05-04 14:46:15 +0300

    supercategories added

--- temp.js
+++ temp.js
@@ -1,8 +1,9 @@
-a = {
-  "Покупки домой": {},
-  "Доставка цветов": {},
-  "Утилизация мусора": {},
-  "Уход за животным": {}
-};
-
-console.log(Object.keys(a));
+<section v-for="(items,title) in categories">
+  <h5>{{title}}</h5>
+<div class="job-line row flex-row flex-nowrap" >
+  <div v-for="(item,i) in items" class="job-item col-5">
+  <h4>{{item.title}}</h4>
+<span>{{item.price}}p.</span>
+</div>
+</div>
+</section>
