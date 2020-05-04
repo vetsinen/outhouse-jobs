299ef076153323ef5706363930b6877a33003135
Maxim <max18071995@mail.ru>
2020-05-04 14:06:22 +0300

    Обновлена главная страница

--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,183 @@
 <template>
-  <div class="container-fluid">
-    <p>
-      Выберите категорию услуг
-    </p>
+    <div class="container-fluid">
+        <p class="h1">
+            Выберите категорию услуг
+        </p>
 
-    <!--      <div class="card col-5 text-white bg-primary mb-3" style="max-width: 18rem;">-->
-    <!--        <div class="card-header"> </div>-->
-    <!--        <div class="card-body">-->
-    <!--          <h5 class="card-title" style="white-space: nowrap"></h5>-->
-    <!--          <p class="card-text" style="white-space: nowrap" >Продукты от диетолога</p>-->
-    <!--          <p class="card-text">-->
-    <!--            500p.</p>-->
-    <!--        </div>-->
-    <!--      </div>-->
+        <!--      <div class="card col-5 text-white bg-primary mb-3" style="max-width: 18rem;">-->
+        <!--        <div class="card-header"> </div>-->
+        <!--        <div class="card-body">-->
+        <!--          <h5 class="card-title" style="white-space: nowrap"></h5>-->
+        <!--          <p class="card-text" style="white-space: nowrap" >Продукты от диетолога</p>-->
+        <!--          <p class="card-text">-->
+        <!--            500p.</p>-->
+        <!--        </div>-->
+        <!--      </div>-->
 
-    <div class="job-line row flex-row flex-nowrap">
-      <div v-for="item in items" class="job-item col-5">
-        <p>{{item}}</p>
-        <p>500p.</p>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="card col-12 text-white bg-primary mb-3" style="min-width: 90%;">
-        <div class="card-header"></div>
-        <div class="card-body">
-          <h5 class="card-title">Побегать с вашей черепахой</h5>
-          <p class="card-text">100р.</p>
+        <div v-for="catalog in catalogs" class="my-3">
+            <p class="h2">{{catalog.name}}</p>
+            <div class="job-line row flex-row flex-nowrap text-white">
+                <div v-for="service in catalog.services" class="job-item col-5 py-3 position-relative">
+                    <p class="h2">{{service.name}}</p>
+                    <p></p>
+                    <p class="position-absolute" style="bottom: 0; right: 0">{{service.price}}p.</p>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="my-5 py-5">
+            <p></p>
+        </div>
     </div>
-
-    <div class="job-line row flex-row flex-nowrap">
-      <div class="job-item col-7">
-        <p>Продукты от диетолога</p>
-        <p>500p.</p>
-      </div>
-
-      <div class="job-item col">
-        <p>Продукты от диетолога</p>
-        <p>500p.</p>
-      </div>
-
-      <div class="job-item col">
-        <p>Продукты от диетолога</p>
-        <p>500p.</p>
-      </div>
-    </div>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cupiditate debitis exercitationem fuga hic laborum
-      optio pariatur quisquam similique sit? Aliquid assumenda at dicta distinctio itaque labore, natus, optio porro qui
-      recusandae sint soluta sunt? Ad, aliquam animi assumenda consequuntur corporis culpa delectus deleniti dolorum
-      eligendi enim exercitationem expedita fuga fugit harum laboriosam modi mollitia necessitatibus nisi non odit
-      pariatur praesentium provident quam quasi qui, quibusdam quisquam rem saepe sapiente tempore ullam ut velit
-      voluptate. Animi asperiores aspernatur, assumenda commodi corporis culpa dignissimos dolore excepturi id ipsam
-      iste labore laboriosam magni pariatur quia rem rerum sit! Eos ipsam nam placeat.
-    </p>
-  </div>
 </template>
 
 <script>
 
-  export default {
-    components: {},
-    data() {
-      return {
-        value: 42,
-        items: ['Продукты от диетолога', 'Продукты от шеф-повара', 'Продукты от гастронома №4']
-      }
+    export default {
+        components: {},
+        data() {
+            return {
+                value: 42,
+                catalogs: [
+                    {
+                        name: 'Покупки домой',
+                        services: [
+                            {
+                                name: 'Продукты по списку или под рецепт',
+                                description: '"Позвольте Дела Вне Дома заняться регулярными покупками. Мы закупим продукты на неделю, доставим необходимые продукты под ваш рецепт или доставим все необходимое для вечеринки.\n' +
+                                'Дела Вне Дома доставляют как из локальных магазинов, так и из крупных торговых центров.\n' +
+                                'Укажите в комментариях к заказу, какие продукты нужно вам доставить."',
+                                price: 200
+                            },
+                            {
+                                name: 'Набор продуктов №1',
+                                description: '"В случае, когда требуется оперативно подготовиться к визиту гостей или не хочется заморачиваться с составлением списков\n' +
+                                '\n' +
+                                'Бакалея (при нажатии – открывается описание поверх страницы)\n' +
+                                'Сахар Русский песок ГОСТ 3222-2015, 1кг 1 шт\n' +
+                                'Мука пшеничная Super хлебопекарная высшего сорта 2кг 1 шт\n' +
+                                'Масло Super Оливковое Extra Virgin 0,5л 1 шт\n' +
+                                'Гречка Super ядрица в варочных пакетах 5*80г 1 шт\n' +
+                                'Рис Мистраль Кубань круглозерный 900г 1 шт\n' +
+                                'Макаронные изделия Шебекинские Спирали 450г 1 шт\n' +
+                                'Горошек Bonduelle зеленый нежный 200г 1 шт\n' +
+                                'Кукуруза Bonduelle сладкая 150г 2 шт\n' +
+                                'Фасоль Heinz в томатном соусе 415 г 1 шт"',
+                                price: 300
+                            },
+                            {
+                                name: 'Набор продуктов №2',
+                                description: '"Свежее\n' +
+                                'Мякоть бедра Мираторг Black Angus охлажденная в вакуумной упаковке 800 г 1 шт\n' +
+                                'Хлеб Harry\'s American Sandwich Сандвичный пшеничный 470г 1 шт\n' +
+                                'Молоко Parmalat ультрапастеризованное 3.5% 1л 1 шт\n' +
+                                'Кефир Домик в деревне 3.2% 1 кг 1 шт\n' +
+                                'Сметана Простоквашино 15% 315 г 1 шт\n' +
+                                'Творог Простоквашино 5% 220 г 1 шт\n' +
+                                'Биойогурт Danone Активиа натуральная 3.5% 150 г 4 шт\n' +
+                                'Масло Экомилк сливочное 82.5% 180 г 1 шт\n' +
+                                'Майонез Слобода Оливковый 67% 230мл 1 шт\n' +
+                                'Сыр Valio Голландский полутвердый 45% нарезка 120г 1 шт"',
+                                price: 300
+                            },
+                            {
+                                name: 'Набор "Чистящие средства и хозяйственные товары" ',
+                                description: '"Оперативно доставим чистящие средства для ежедневного ухода за домом или генеральной уборки\n' +
+                                '\n' +
+                                'Список средств:"',
+                                price: 300
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Доставка цветов',
+                        services: [
+                            {
+                                name: 'Розы домой',
+                                description: 'Если потребовалось украсить дом или собрать букет на мероприятие, Дела Вне Дома оперативно доставят цветы.',
+                                price: 300
+                            },
+                            {
+                                name: 'Тюльпаны домой',
+                                description: 'Если потребовалось украсить дом или собрать букет на мероприятие, Дела Вне Дома оперативно доставят цветы.',
+                                price: 300
+                            },
+                            {
+                                name: 'Гвоздики домой',
+                                description: 'Если потребовалось украсить дом или собрать букет на мероприятие, Дела Вне Дома оперативно доставят цветы.',
+                                price: 300
+                            },
+                            {
+                                name: 'Купить и доставить другие цветы',
+                                description: 'Если Вам требуется что-то особенное, что не укладывается в одну из опций, смело указывайте что вам требуется',
+                                price: 300
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Утилизация мусора',
+                        services: [
+                            {
+                                name: 'Вынос ежедневного мусора',
+                                description: 'Наш помощник заберет ежедневный мусор от вашей двери или с лестничной клетки, достаточно просто сообщить как попасть в подъезд и мусор будет вынесен.',
+                                price: 150
+                            },
+                            {
+                                name: 'Вывоз хлама и объемного мусора',
+                                description: '"Если у Вас накопилось много лишнего в квартире, будем рады оперативно утилизировать ваш мусор. \n' +
+                                '\n' +
+                                'Опишите в комментарии к заказу какие габариты мусора"',
+                                price: 250
+                            },
+                        ]
+                    },
+                    {
+                        name: 'Уход за животным',
+                        services: [
+                            {
+                                name: 'Погулять с собакой',
+                                description: 'Мы с огромным удовольствием выведем на прогулку вашу собаку.',
+                                price: 150
+                            },
+                            {
+                                name: 'Купить корм и товары для питомца',
+                                description: '"Укажите что потребуется купить вашему питомцу.\n' +
+                                '\n' +
+                                'Составьте список покупок и напишите его в комментарии к заказу."',
+                                price: 250
+                            },
+                        ]
+                    }
+                ]
+            }
+        }
     }
-  }
 </script>
 
 <style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+    .container {
+        margin: 0 auto;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
 
-  .job-line {
-    overflow-y: auto;
-    /*-ms-overflow-style: none;*/
-  }
+    .job-line {
+        overflow-y: auto;
+        /*-ms-overflow-style: none;*/
+    }
 
-  /*.job-line::-webkit-scrollbar {*/
-  /*  display: none;*/
-  /*}*/
+    /*.job-line::-webkit-scrollbar {*/
+    /*  display: none;*/
+    /*}*/
 
-  .job-item {
-    word-break: normal;
-    background-color: dodgerblue;
-    border-radius: 10px;
-  }
+    .job-item {
+        word-break: normal;
+        background-color: dodgerblue;
+        border-radius: 10px;
+        min-width: 300px;
+    }
 
 
 </style>
