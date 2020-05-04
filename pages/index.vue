1de23a1dd4837a7fa564277e2bd0cf749f121384
vetsinen <vetsinen@gmail.com>
2020-05-04 11:41:08 +0300

    dynamic vue template

--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,22 @@
       Выберите категорию услуг
     </p>
 
+    <!--      <div class="card col-5 text-white bg-primary mb-3" style="max-width: 18rem;">-->
+    <!--        <div class="card-header"> </div>-->
+    <!--        <div class="card-body">-->
+    <!--          <h5 class="card-title" style="white-space: nowrap"></h5>-->
+    <!--          <p class="card-text" style="white-space: nowrap" >Продукты от диетолога</p>-->
+    <!--          <p class="card-text">-->
+    <!--            500p.</p>-->
+    <!--        </div>-->
+    <!--      </div>-->
+
     <div class="job-line row flex-row flex-nowrap">
-      <div class="job-item col-5">
-        <p>Продукты от диетолога</p>
-        <p>500p.</p>
-      </div>
-
-      <div class="job-item col-5">
-        <p>Продукты от диетолога</p>
-        <p>500p.</p>
-      </div>
-
-      <div class="job-item col-5">
-        <p>Продукты от диетолога</p>
+      <div v-for="item in items" class="job-item col-5">
+        <p>{{item}}</p>
         <p>500p.</p>
       </div>
     </div>
-
-      <!--      <div class="card col-5 text-white bg-primary mb-3" style="max-width: 18rem;">-->
-      <!--        <div class="card-header"> </div>-->
-      <!--        <div class="card-body">-->
-      <!--          <h5 class="card-title" style="white-space: nowrap"></h5>-->
-      <!--          <p class="card-text" style="white-space: nowrap" >Продукты от диетолога</p>-->
-      <!--          <p class="card-text">-->
-      <!--            500p.</p>-->
-      <!--        </div>-->
-      <!--      </div>-->
-
-
-
 
     <div class="row">
       <div class="card col-12 text-white bg-primary mb-3" style="min-width: 90%;">
@@ -45,24 +32,30 @@
     </div>
 
     <div class="job-line row flex-row flex-nowrap">
-      <div class="job-item col-5">
+      <div class="job-item col-7">
         <p>Продукты от диетолога</p>
         <p>500p.</p>
       </div>
 
-      <div class="job-item col-5">
+      <div class="job-item col">
         <p>Продукты от диетолога</p>
         <p>500p.</p>
       </div>
 
-      <div class="job-item col-5">
+      <div class="job-item col">
         <p>Продукты от диетолога</p>
         <p>500p.</p>
       </div>
     </div>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cupiditate debitis exercitationem fuga hic laborum optio pariatur quisquam similique sit? Aliquid assumenda at dicta distinctio itaque labore, natus, optio porro qui recusandae sint soluta sunt? Ad, aliquam animi assumenda consequuntur corporis culpa delectus deleniti dolorum eligendi enim exercitationem expedita fuga fugit harum laboriosam modi mollitia necessitatibus nisi non odit pariatur praesentium provident quam quasi qui, quibusdam quisquam rem saepe sapiente tempore ullam ut velit voluptate. Animi asperiores aspernatur, assumenda commodi corporis culpa dignissimos dolore excepturi id ipsam iste labore laboriosam magni pariatur quia rem rerum sit! Eos ipsam nam placeat.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cupiditate debitis exercitationem fuga hic laborum
+      optio pariatur quisquam similique sit? Aliquid assumenda at dicta distinctio itaque labore, natus, optio porro qui
+      recusandae sint soluta sunt? Ad, aliquam animi assumenda consequuntur corporis culpa delectus deleniti dolorum
+      eligendi enim exercitationem expedita fuga fugit harum laboriosam modi mollitia necessitatibus nisi non odit
+      pariatur praesentium provident quam quasi qui, quibusdam quisquam rem saepe sapiente tempore ullam ut velit
+      voluptate. Animi asperiores aspernatur, assumenda commodi corporis culpa dignissimos dolore excepturi id ipsam
+      iste labore laboriosam magni pariatur quia rem rerum sit! Eos ipsam nam placeat.
     </p>
   </div>
 </template>
@@ -70,7 +63,13 @@
 <script>
 
   export default {
-    components: {}
+    components: {},
+    data() {
+      return {
+        value: 42,
+        items: ['Продукты от диетолога', 'Продукты от шеф-повара', 'Продукты от гастронома №4']
+      }
+    }
   }
 </script>
 
@@ -88,17 +87,16 @@
     overflow-y: auto;
     /*-ms-overflow-style: none;*/
   }
-  .job-line::-webkit-scrollbar {
-    display: none;
-  }
+
+  /*.job-line::-webkit-scrollbar {*/
+  /*  display: none;*/
+  /*}*/
 
   .job-item {
     word-break: normal;
     background-color: dodgerblue;
     border-radius: 10px;
   }
-
-
 
 
 </style>
