be09c8c9136888f9d2aef0973332d402e5046480
vetsinen <vetsinen@gmail.com>
2020-05-04 08:01:10 +0300

    before navigation change

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav class="fixed-bottom navbar-expand navbar-light bg-light">
+      <div>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
+              Catalogue
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link active-class="active" class="nav-link" to="/orders">
+              Orders
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link active-class="active" class="nav-link" to="/profile">
+              Profile
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 5px;
+}
+
+</style>
